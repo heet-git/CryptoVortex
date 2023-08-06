4a8a820ed4cc6351ec3ba7009d832f0beb6f5023
heet-git <58705043+heet-git@users.noreply.github.com>
2023-08-06 18:55:45 -0400

    Add sign in and sign up buttons

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,34 @@ import React from "react"
 import Navbar from "./common/Navbar"
 import { ThemeProvider, createTheme } from "@mui/material"
 
+
 const Theme = createTheme({
 typography: {
     "fontFamily": `'DM Sans', "Roboto", "sans-serif"`,
-    "Color": "black"
 },
 palette: {
     primary: {
-      main: '#FFF',
+      main: '#fff',
     },
-    text:{
-      primary: '#000'
-    },
+    text: {
+      primary: '#fff'
+    }
+  },
+  components: {
+    MuiTypography: {
+      styleOverrides: {
+        root: {
+          color: 'black'
+        }
+      }
+    }
   },
 })
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
-        <Navbar/>
+        <Navbar />
     </ThemeProvider>
   )
 }
--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -1,14 +1,15 @@
-import * as React from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
-import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+import MenuSharpIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
+import CycloneIcon from '@mui/icons-material/Cyclone';
 
 const pages = ['Explore', 'Learn', 'News', 'About'];
 
@@ -24,7 +25,7 @@ function Navbar() {
     };
 
     return (
-        <AppBar position="static">
+        <AppBar position="static" sx={{borderBottom: 'none'}}>
             <Container maxWidth="xl">
             <Toolbar disableGutters>
                 <Typography
@@ -35,15 +36,16 @@ function Navbar() {
                 sx={{
                     mr: 2,
                     display: { xs: "none", md: "flex" },
+                    alignItems: "center",
                     fontWeight: 700,
-                    color: "black",
+                    color: "Primary",
                     textDecoration: "none",
                 }}
                 >
-                CryptoVortex
+                CryptoV<CycloneIcon/>rtex
                 </Typography>
 
-                <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, padding: 0, margin: 0, color: 'black'}}>
+                <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, padding: 0, margin: 0}}>
                 <IconButton
                     size="small"
                     aria-haspopup="true"
@@ -53,7 +55,6 @@ function Navbar() {
                     <MenuSharpIcon />
                 </IconButton>
                 <Menu
-                    id="menu-appbar"
                     anchorEl={anchorElNav}
                     anchorOrigin={{
                         vertical: "bottom",
@@ -68,17 +69,28 @@ function Navbar() {
                     onClose={handleCloseNavMenu}
                     sx={{
                     display: { xs: "block", md: "none" },
+                    justifyContent: 'center'
                     }}
                 >
                     {pages.map((page) => (
-                    <MenuItem key={page} onClick={handleCloseNavMenu} 
+                    <MenuItem key={page} onClick={handleCloseNavMenu}
                     >
-                        <Typography textAlign="center">{page}</Typography>  /// here is the issue. 
+                        <Typography 
+                        textAlign="center"  
+                        sx={{color:'black'}}>
+                        {page}
+                        </Typography>
                     </MenuItem>
                     ))}
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Button variant='text' sx={{color:'black'}}>Sign in</Button>
+                    </MenuItem>
+                    <MenuItem onClick={handleCloseNavMenu}>
+                        <Button variant='contained'>Get started</Button>
+                    </MenuItem>
+                    
                 </Menu>
                 </Box>
-
                 {/* Mobile View */}
 
                 <Typography
@@ -89,6 +101,7 @@ function Navbar() {
                     sx={{
                         mr: 1,
                         display: { xs: "flex", md: "none" },
+                        alignItems:"center",
                         flexGrow: 1,
                         fontFamily: "monospace",
                         fontWeight: 700,
@@ -96,19 +109,23 @@ function Navbar() {
                         textDecoration: "none",
                     }}
                 >
-                    CryptoVortex
+                    CryptoV<CycloneIcon sx={{ m:0, p:0}}/>rtex
                 </Typography>
                 
-                <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }, justifyContent: 'center'}}>
                     {pages.map((page) => (
                     <Button
                     key={page}
                     onClick={handleCloseNavMenu}
-                    sx={{ my: 2, color: "white", display: "block" }}
+                    sx={{ my: 2, color: "black", display: "block"}}
                     >
                     {page}
                     </Button>
                     ))}
+                </Box>
+                <Box sx={{display: {xs: "none", md:"block"} }} >
+                <Button variant='text' sx={{color:'black'}}>Sign in</Button>
+                <Button variant='contained'>Get started</Button>
                 </Box>
 
             </Toolbar>
