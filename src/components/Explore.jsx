c102fcfdbb7e8df7c079dbbb4432f7c2d0be9c81
heet-git <58705043+heet-git@users.noreply.github.com>
2023-08-08 07:21:08 -0400

    add intro text
    add intro and set home image in background.

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import homeImgLight from "/public/home-img-light.jpg"
 import homeImgDark from "/public/home-img-dark.jpg"
 
@@ -7,16 +7,30 @@ function Explore(){
     return(
         <Container maxWidth="xl" disableGutters>
             <Box
-                component="img"
-                src={homeImgLight}
                 sx={{
-                    width:"100%",
-                    objectFit: "cover",
-                    objectPosition: "center",
-                    height: 500,
-                    display:{xs: "none", md: "flex"}
+                    backgroundImage: `url(${homeImgLight})`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    backgroundColor: "white",
+                    minHeight: 500,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    padding: 5,
+                    textAlign: "center",
+                    display:{xs: "none", md: "flex"},
+                    border: '1px solid red'
                     }}
-            />
+            >
+                <Typography
+                    variant="h4"
+                    sx={{border: '1px solid red'}}
+                >
+                    Dive into the CryptoVortex: A Whirlwind of Trading Opportunities
+                </Typography>
+
+            </Box>
             <Box
                 component="img"
                 src={homeImgLight}
