d667fbfacde05ff7d16c8189bba176bc0e3a153b
heet-git <58705043+heet-git@users.noreply.github.com>
2023-08-09 22:21:56 -0400

    Add site data on Explore

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { Box, Container, Typography } from "@mui/material";
 import Stack from "@mui/material/Stack";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+
 import homeImgLight from "/public/home-img-light.jpg"
 import homeImgDark from "/public/home-img-dark.jpg"
 
@@ -19,7 +24,6 @@ function Explore(){
                     justifyContent: "center",
                     padding: 5,
                     textAlign: "center",
-                    display:{xs: "none", md: "flex"},
                     }}
             >
                 <Typography
@@ -35,15 +39,127 @@ function Explore(){
 
             </Box>
 
-            {/* <Stack 
-                direction={{ xs: 'column', sm: 'row' }} spacing={{ xs: 1, sm: 2, md: 4 }}> */}
-                <Typography variant="h6" color="primary">Item 1</Typography>
-                <Typography variant="h6" color="primary">Item 2</Typography>
-                <Typography variant="h6" color="primary">Item 3</Typography>
-            {/* </Stack> */}
+            <Stack 
+                direction={{ xs: 'column', sm: 'row' }} 
+                spacing={{ xs: 1, sm: 2, md: 4 }}
+                sx={{
+                justifyContent:"space-evenly",
+                py: 4,
+                m: 1,
+                }}
+                >
+                <Box>
+                    <Typography variant="h4">$1 billion</Typography>
+                    <Typography variant="body1">Quarterly volume traded</Typography>
+                </Box>
+                <Box>
+                    <Typography variant="h4">20+</Typography>
+                    <Typography variant="body1">Cryptocurrencies listed</Typography>
+                </Box>
+                <Box>
+                    <Typography variant="h4">1 million</Typography>
+                    <Typography variant="body1">Registered users</Typography>
+                </Box>
+                <Box>
+                    <Typography variant="h4">&lt;0.1%</Typography>
+                    <Typography variant="body1">Lowest transaction fees</Typography>
+                </Box>
+            </Stack>
 
+            <Box>
+                <Typography
+                variant="h4"
+                >Trending</Typography>
+            
+            <Stack
+                direction={{ xs: 'column', sm: 'row' }} 
+                spacing={{ xs: 1, sm: 2, md: 4 }}
+                sx={{
+                    justifyContent:"space-evenly",
+                    py: 4,
+                    m: 1,
+                }}
+            >
+            
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Coin
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Coin info
+                    </Typography>
+                </CardContent>
+                </CardActionArea>
+            </Card>
+
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Coin
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Coin info
+                    </Typography>
+                </CardContent>
+                </CardActionArea>
+            </Card>
+
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Coin
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Coin info
+                    </Typography>
+                </CardContent>
+                </CardActionArea>
+            </Card>
+
+            <Card sx={{ maxWidth: 345 }}>
+                <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Coin
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Coin info
+                    </Typography>
+                </CardContent>
+                </CardActionArea>
+            </Card>
+            </Stack>
+            </Box>
             {/* Mobile view */}
-            <Box
+            {/* <Box
                 component="img"
                 src={homeImgLight}
                 sx={{
@@ -53,7 +169,18 @@ function Explore(){
                     height: 500,
                     display:{md: "none", xs: "flex"}
                     }}
-            />
+            >
+                <Typography
+                    variant="h3"
+                    sx={{
+                        fontWeight: "600",
+                        maxWidth: "lg"
+                    }}
+                >
+                    Dive into the CryptoVortex: 
+                    A Whirlwind of Trading Opportunities
+                </Typography>
+            </Box>         */}
         </Container>
     )
 }
