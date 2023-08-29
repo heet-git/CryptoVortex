f6a20f943994fafdf4a448556ef75b5abf1c1c6f
heet-git <58705043+heet-git@users.noreply.github.com>
2023-08-28 22:37:52 -0400

    change news api

--- src/assets/Api.js
+++ src/assets/Api.js
@@ -1,6 +1,6 @@
     const globalDataUrl = 'https://api.coingecko.com/api/v3/global'
 
-    const coinsMarketurl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C1y&locale=en&precision=2'
+    const coinsMarketUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d%2C1y&locale=en&precision=2'
 
     async function getMarketdata(){
         const response = await fetch(globalDataUrl)
@@ -16,7 +16,7 @@
     }
     
     async function getCoins() {
-        const response = await fetch(coinsMarketurl);
+        const response = await fetch(coinsMarketUrl);
         if (!response.ok) {
             throw new Error({
                 message: "failed to fetch coins data",
@@ -28,24 +28,16 @@
         return data
     } 
 
-    const url = 'https://bing-news-search1.p.rapidapi.com/news?category=Cryptocurrency&safeSearch=Off&textFormat=Raw';
-    const options = {
-        method: 'GET',
-        headers: {
-        'X-BingApis-SDK': 'true',
-        'X-RapidAPI-Key': 'c57e80cd26msh5898b169528186fp1f068fjsn8af7bd3d4369',
-        'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
-        }
-    };
+    const cryptoNewsUrl = 'https://newsdata.io/api/1/news?apikey=pub_28435bef511fe731b7803eb428ee813e64e4e&q=cryptocurrencies';
         
     async function cryptoNewsData() {
         try {
-        const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
+            const response = await fetch(cryptoNewsUrl);
+            const result = await response.json();
+            return result
         } catch (error) {
-        console.error(error);
-        }
+            throw error
+        }   
     }
         
     export {getMarketdata, getCoins, cryptoNewsData}
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions, Container } from '@mui/material';
+
+
 import { cryptoNewsData } from '../assets/Api';
 
 function News() {
-    const [cryptoNews, setCryptoNews] = useState({})
+    const [cryptoNews, setCryptoNews] = useState([])
 
     useEffect(()=> {
         const newsData = async () =>{
@@ -16,9 +23,37 @@ function News() {
         newsData()
     }, [])
 
+
 return (
-    <div>news</div>
+    <Container>
+    {cryptoNews.map((news, index) => (
+        <Card key={index} sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+            <CardMedia
+            component="img"
+            height="140"
+            image={news.results.image_url}
+            alt="green iguana"
+            />
+            <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+                {news.results.title}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+                {news.results.description}
+            </Typography>
+            </CardContent>
+        </CardActionArea>
+        <CardActions>
+            <Button size="small" color="primary">
+            More..
+            </Button>
+        </CardActions>
+        </Card>
+    ))}
+    </Container>
 );
+
 }
 
 export default News;
