51b08570c8f90b9a63f4c479b026542c2437813e
heet-git <58705043+heet-git@users.noreply.github.com>
2023-09-04 18:59:37 -0400

    Add about page
    Add about page. Set redirects to fix "page not found" error on netlify

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,13 @@ import Theme from "./assets/Theme"
 import Home from "./components/Home"
 import Explore from "./components/Explore"
 import News from "./components/News"
-
+import About from "./components/About"
 const allRoutes = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="explore" element={<Explore/>} />
         <Route path="news" element={<News/>} />
+        <Route path="about" element={<About/>} />
     </Route>
 ))
 
