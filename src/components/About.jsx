e73a1efef11cb4487eab7cd222b8157a242441b0
heet-git <58705043+heet-git@users.noreply.github.com>
2023-09-07 07:13:54 -0400

    add and style content in about
    Add intro image and text. apply styling.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,26 +6,39 @@ function About(props) {
   return (
     <Container>
       <Box
-        textAlign="center"
-        my="5rem"
+        display="flex"
+        alignItems="center"
+        my= "5rem"
       >
-        <Typography variant="h4"
-          sx={{
-            fontWeight: "600"
-          }}
-        >
-          About CryptoVortex
-        </Typography>
-      </Box>
-      <Box>
         <Box
+          component="img"
+          src= {aboutImg}
           sx={{
-            backgroundImage: `url(${aboutImg})`,
-            
+            height: "100%",
+            width: "50%"
           }}
+        ></Box>
+        <Box
+          display="flex"   
+          flexDirection="column"
+          alignItems="center"
+          px="3rem"
         >
-
-        </Box>
+          <Typography variant="h4"
+            sx={{
+              fontWeight: "600"
+            }}
+          >
+            About CryptoVortex
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            textAlign="center"
+            pt="1rem"
+          >
+            In a rapidly evolving digital landscape, cryptocurrency has emerged as the future of finance. At CryptoVortex, we understand the intricate tapestry of blockchain technology and the potential of decentralized finance. Our mission? To provide a seamless and secure trading platform that empowers both novice and experienced traders.
+          </Typography>
+      </Box>
       </Box>
 
     </Container>
